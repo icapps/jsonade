fe0adf2d3d61cb04d1a47d5a8caeaa27538c51ff
hoetmaaiers <robin.houdmeyers@icapps.com>
2017-11-07 21:38:20 +0100

    Add array serialisation

--- tests/serializer.test.js
+++ tests/serializer.test.js
@@ -60,6 +60,42 @@ describe('Serializer', () => {
     });
   });
 
+  test('should serialze a nested array', () => {
+    // raw data
+    const rawData = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 27,
+      hobbies: [
+        { id: 1, name: 'Bowling', description: 'sport & stuff' },
+        { id: 2, name: 'Reading', type: 'read & stuff' },
+        { id: 3, name: 'Gardening', type: 'plants & stuff' },
+      ],
+    };
+
+    // serializer definition
+    const userSerializer = new Serializer('user', {
+      attributes: ['firstName', 'lastName', 'hobbies'],
+      hobbies: {
+        attributes: ['name'],
+      },
+    });
+
+    const result = userSerializer.serialize(rawData);
+    const { meta, data } = result;
+
+    expect(meta.type).toEqual('user');
+    expect(data).toEqual({
+      firstName: 'John',
+      lastName: 'Doe',
+      hobbies: [
+        { name: 'Bowling' },
+        { name: 'Reading' },
+        { name: 'Gardening' },
+      ],
+    });
+  });
+
   test('should throw an exception when no resource is given', () => {
     expect(() => {
       new Serializer(null, {
