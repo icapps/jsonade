dcc7f705b977824237aac0579d7b40b46b1170b6
hoetmaaiers <robin.houdmeyers@icapps.com>
2018-01-10 14:57:52 +0100

    Add test for nested serializer on a list of data

--- tests/nested-serializer.test.js
+++ tests/nested-serializer.test.js
@@ -38,4 +38,54 @@ describe('Nested serializer', () => {
       },
     });
   });
+
+  test('should succesfully use a nested serializer on a list of data', () => {
+    // raw data
+    const rawData = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 27,
+      address: [
+        {
+          street: 'Markt',
+          number: '100',
+          city: 'Zonnedorp',
+          country: 'Belgium',
+        }, {
+          street: 'Kerkstraat',
+          number: '69',
+          city: 'Zonnedorp',
+          country: 'Belgium',
+        },
+      ]
+    };
+
+    // serializer definition
+    const addressSerializer = new Serializer('address', {
+      attributes: ['street', 'number'],
+    });
+
+    const userSerializer = new Serializer('user', {
+      attributes: ['firstName', 'lastName', 'address'],
+      address: addressSerializer,
+    });
+
+    const result = userSerializer.serialize(rawData);
+    const { meta, data } = result;
+
+    expect(meta.type).toEqual('user');
+    expect(data).toEqual({
+      firstName: 'John',
+      lastName: 'Doe',
+      address: [
+        {
+          street: 'Markt',
+          number: '100',
+        }, {
+          street: 'Kerkstraat',
+          number: '69',
+        },
+      ],
+    });
+  });
 });
